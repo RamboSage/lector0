0170d2071ae1a1c7476ede6629f68eb19969f773
Mbayoo Mungunda <mbayoomungunda@Mbayoos-MacBook-Pro.local>
2020-05-13 21:27:37 +0100

    inheritance

--- python.py
+++ python.py
@@ -5,3 +5,18 @@ for n in range(2):
     print (n)
 
 s = set()
+
+def square(x):
+    return x * x
+
+for i in range(10):
+    print("{} squared is {}".format(i,square(i)))
+
+class Point:
+    def _init_(self,x,y):
+        self.x = x
+        self.y = y
+
+p = Point(3, 5)
+print(p.x)
+print(p.y)
